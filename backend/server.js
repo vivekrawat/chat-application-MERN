137c80288db2bdd512e824b7ffa2e5c4bbdfb31d
vivekrawat <vivekrawat7534@gmail.com>
2022-12-13 20:33:36 +0530

    feat/added-login-signup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db")
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+var cors = require("cors");
 
 dotenv.config();
 const app = express();
+connectDB()
 
 app.use(express.json()); // to accept json data
-var cors = require("cors");
 app.use(cors());
+app.use("/api/user", userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
 app.get("/", (req, res) => {
   res.send("API Running!");
 });
