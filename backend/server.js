1e453190cc92d799826ad1cf3d1b8c2fb02fa372
vivekrawat <vivekrawat7534@gmail.com>
2022-12-19 11:16:32 +0530

    feat/messages-routes

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db")
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes")
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 var cors = require("cors");
+const { application } = require("express");
 
 dotenv.config();
 const app = express();
@@ -15,6 +17,7 @@ app.use(express.json()); // to accept json data
 app.use(cors());
 app.use("/api/user", userRoutes)
 app.use("/api/chat", chatRoutes)
+app.use("/api/message", messageRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
